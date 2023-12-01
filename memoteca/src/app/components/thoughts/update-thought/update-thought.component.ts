6a7c978c307ff6b2bb3fd15ba90682684745ae58
Carol-Rocha <caroldsr1@gmail.com>
2023-12-01 19:15:35 -0300

    fix: create reactive forms for components create new thoughts and update

--- memoteca/src/app/components/thoughts/update-thought/update-thought.component.ts
+++ memoteca/src/app/components/thoughts/update-thought/update-thought.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { IThought } from '../thought';
 import { ThoughtService } from '../thought.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { onlyLowercaseLetters } from '../create-thoughts/thoughts-validators';
 
 @Component({
   selector: 'app-update-thought',
@@ -9,34 +10,59 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./update-thought.component.css'],
 })
 export class UpdateThoughtComponent implements OnInit {
-  thought: IThought = {
-    conteudo: '',
-    autoria: '',
-    modelo: '',
-  };
+  form!: FormGroup;
 
   constructor(
     private service: ThoughtService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.service.getThoughtById(parseInt(id)).subscribe((pensamento) => {
-        this.thought = pensamento;
+        this.form = this.formBuilder.group({
+          id: [pensamento.id],
+          conteudo: [
+            pensamento.conteudo,
+            Validators.compose([
+              Validators.required,
+              Validators.pattern(/(.|\s)*\S(.|\s)*/),
+            ]),
+          ],
+          autoria: [
+            pensamento.autoria,
+            Validators.compose([
+              Validators.required,
+              Validators.minLength(3),
+              onlyLowercaseLetters,
+            ]),
+          ],
+          modelo: [pensamento.modelo],
+        });
       });
     }
   }
 
   updateThought() {
-    this.service.updateThought(this.thought).subscribe(() => {
-      this.router.navigate(['/listar-pensamentos']);
-    });
+    if (this.form.valid) {
+      this.service.updateThought(this.form.value).subscribe(() => {
+        this.router.navigate(['/listar-pensamentos']);
+      });
+    }
   }
 
   cancelButton() {
     this.router.navigate(['/listar-pensamentos']);
+  }
+
+  enableButton(): string {
+    if (this.form.valid) {
+      return 'botao';
+    } else {
+      return 'botao__desabilitado';
+    }
   }
 }
